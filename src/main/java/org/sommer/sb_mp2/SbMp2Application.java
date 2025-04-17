7202c364f145f5fa17789868bb0de88181037fd3
KimSoYeonnn <10sosokim10@gmail.com>
2025-04-17 14:11:50 +0900

    initial commit

---
+++ src/main/java/org/sommer/sb_mp2/SbMp2Application.java
@@ -0,0 +1,13 @@
+package org.sommer.sb_mp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbMp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbMp2Application.class, args);
+	}
+
+}
